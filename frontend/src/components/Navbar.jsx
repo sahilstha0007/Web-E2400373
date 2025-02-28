65c8460ebf9c64e42035b3c4816a38bea760db6c
Abhinav Singh <144584729+Not-Donno@users.noreply.github.com>
2025-02-28 16:54:38 +0545

    darktheme

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaBookOpen } from 'react-icons/fa';
+import { FaBookOpen, FaSun, FaMoon } from 'react-icons/fa';
 import defaultPFP from '../assets/non.png'; 
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+  // Apply the theme to the body
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [theme]);
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-white flex justify-between items-center px-8 py-4 z-50">
+    <header className="fixed top-0 left-0 w-full bg-white dark:bg-gray-800 flex justify-between items-center px-8 py-4 z-50">
       <div className="flex items-center gap-6 font-bold text-xl">
         <p className="text-orange-500 text-2xl">MVIC</p>
         <nav className="flex gap-6 font-light">
@@ -44,13 +59,16 @@ const Navbar = () => {
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           />
           {isMenuOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-gray-200 shadow-md rounded-md p-4">
-            <div className="absolute -top-2 right-4 w-4 h-4 bg-gray-150 rotate-45"></div>
-            <button className="w-full text-left py-2 px-4 hover:bg-orange-100">Sign In</button>
-            <button className="w-full text-left py-2 px-4 hover:bg-orange-100">Log In</button>
+            <div className="absolute right-0 mt-2 w-48 bg-gray-200 dark:bg-gray-700 shadow-md rounded-md p-4">
+              <div className="absolute -top-2 right-4 w-4 h-4 bg-gray-200 dark:bg-gray-700 rotate-45"></div>
+              <button className="w-full text-left py-2 px-4 hover:bg-orange-100">Sign In</button>
+              <button className="w-full text-left py-2 px-4 hover:bg-orange-100">Log In</button>
             </div>
           )}
         </div>
+        <button onClick={toggleTheme} className="text-xl">
+          {theme === 'light' ? <FaMoon /> : <FaSun />}
+        </button>
       </div>
     </header>
   );
