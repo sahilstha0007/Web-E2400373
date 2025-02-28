a284a8de2d588e3b1797d6534ad38ce359bdc049
Sahil Shrestha <146646189+sahilstha0007@users.noreply.github.com>
2025-02-28 08:50:27 +0545

    .

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,9 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
+
     <header className="header flex px-8 py-4">
+      
       <div className="w-36 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
         <p className="blue-gradient_text">MVIC</p>
       </div>
@@ -16,14 +18,14 @@ const Navbar = () => {
           Home
         </NavLink>
         <NavLink
-          to=""
+          to="/"
           className={({ isActive }) =>
             isActive ? 'text-blue-500 font-semibold' : 'text-black hover:text-blue-500'}
         >
-         Login
+          Team Members
         </NavLink>
         <NavLink
-          to="/Login"
+          to="/"
           className={({ isActive }) =>
             isActive ? 'text-blue-500 font-semibold' : 'text-black hover:text-blue-500'}
         >
