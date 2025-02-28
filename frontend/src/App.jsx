fb6bd14ed920efaf4d66a7b78b45a5f5990b3715
Abhinav Singh <144584729+Not-Donno@users.noreply.github.com>
2025-02-28 08:26:34 +0545

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import './App.css'
+import Navbar from './components/Navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>test</div> 
+    <Navbar />
   )
 }
 
