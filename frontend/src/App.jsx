c83e97bc723319a0e8c7e7c523e3bed541357f0c
Abhinav Singh <144584729+Not-Donno@users.noreply.github.com>
2025-02-28 08:29:46 +0545

    tset

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,17 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Navbar />
+    <Router>
+      <Navbar className=" sticky top-0 left-0 bg-white"/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/TeamMember" element={<TeamMember />} />
+          <Route path="/AboutTeam" element={<AboutTeam />} />
+          <Route path="/Problems" element={<Problem />} />
+          <Route path="/OurGoal" element={<Goal />} />
+          <Route path="/Solution" element={<Solution />} />
+        </Routes>
+    </Router>
   )
 }
 
