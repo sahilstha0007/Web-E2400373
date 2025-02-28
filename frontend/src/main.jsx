94d59d40a83b401d0270d06fb1d9cf3acc3080ae
Sahil <sahiltuchhe123@gmail.com>
2025-02-28 15:03:08 +0545

    index

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import AuthProvider from './context/authContext'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <AuthProvider>
     <App />
+  </AuthProvider>
   </BrowserRouter>
   
 )
--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,8 +1,15 @@
+import { GraduationCap } from "lucide-react"
+import { Link } from "react-router-dom"
+
 function AuthPage() {
   return (
     <div className="flex flex-col min-h-screen ">
-      <header className="pxl">
-
+      <header className="px-4 lg:px-6 h-14 flex items-center border-b">
+        <Link to={"/"}
+        className="flex items-center justify-center "
+        > 
+        <GraduationCap className="h-8 w- mr-4" />
+        </Link>
       </header>
       
     </div>
