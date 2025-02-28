817b1af3f882300cfd7c4cc37d43a552f03b964a
Sahil <sahiltuchhe123@gmail.com>
2025-02-28 12:57:44 +0545

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,36 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
-import About from './pages/About'
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import About from './pages/About';
+import Home from './pages/Home';
+import Courses from './pages/Courses';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
   const noNavbarRoutes = ['/login', "/"];
 
   return (
-    <Router>
-      <Navbar />
+    <>
+      {!noNavbarRoutes.includes(location.pathname) && (
+        <div className='sticky top-0 z-50 bg-[#AF8F6F]'>
+          <Navbar />
+        </div>
+      )}
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
         <Route path="/courses" element={<Courses />} />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
