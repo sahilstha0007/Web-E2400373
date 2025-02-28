5a77c9d2839e32225d728be290f72fac7f079616
Abhinav Singh <144584729+Not-Donno@users.noreply.github.com>
2025-02-28 08:31:20 +0545

    nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="header flex px-8 py-4">
+      <div className="w-36 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <p className="blue-gradient_text">MVIC</p>
+      </div>
 
-export default Navbar
+      <nav className="flex gap-10">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'text-blue-500 font-semibold' : 'text-black hover:text-blue-500'}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'text-blue-500 font-semibold' : 'text-black hover:text-blue-500'}
+        >
+          Team Members
+        </NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'text-blue-500 font-semibold' : 'text-black hover:text-blue-500'}
+        >
+          About Team
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
