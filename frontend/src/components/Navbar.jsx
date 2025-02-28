3a4c01612bd9b5a0e7e29f39f4f2d754e0152a4e
Sahil Shrestha <146646189+sahilstha0007@users.noreply.github.com>
2025-02-28 08:29:20 +0545

    Nav bar gayab

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-<<<<<<< HEAD
 function Navbar() {
   return (
     <div>
@@ -10,12 +9,3 @@ function Navbar() {
 }
 
 export default Navbar
-=======
-const Navbar = () => {
-  return (
-    <Link href = '/' className="">MVIC</Link>
-  )
-}
-
-export default Navbar
->>>>>>> a361bedb4118ae3eaef7d74153e361899b226f15
