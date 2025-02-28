9f7cf32cc588b68a3b0b684484de5cbad1aeb0ee
Sahil <sahiltuchhe123@gmail.com>
2025-02-28 14:47:48 +0545

    Auth

---
+++ frontend/src/pages/auth/index.jsx
@@ -0,0 +1,12 @@
+function AuthPage() {
+  return (
+    <div className="flex flex-col min-h-screen ">
+      <header className="pxl">
+
+      </header>
+      
+    </div>
+  )
+}
+
+export default AuthPage
