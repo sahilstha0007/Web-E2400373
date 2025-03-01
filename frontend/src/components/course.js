7e4d7f180f6c40bd609f655bab89a36096261eeb
Abhinav Singh <144584729+Not-Donno@users.noreply.github.com>
2025-03-01 16:24:50 +0545

    img fixed

--- frontend/src/components/course.js
+++ frontend/src/components/course.js
@@ -1,31 +1,31 @@
 import bg from '../assets/bg.png'
 const courses = [
     {
-      image: {bg},
+      image: bg,
       name: "Hotel Management Basics",
       desc: "Learn the fundamentals of hotel management.",
       faculty: "BHM",
     },
     {
-      image: {bg},
+      image: bg,
       name: "Business Administration 101",
       desc: "Introduction to business management and leadership.",
       faculty: "BBA",
     },
     {
-      image: {bg},
+      image: bg,
       name: "Introduction to Programming",
       desc: "Learn programming fundamentals using Python.",
       faculty: "BIT",
     },
     {
-      image: {bg},
+      image: bg,
       name: "Web Development",
       desc: "Master front-end and back-end web development.",
       faculty: "BIT",
     },
     {
-      image: {bg},
+      image: bg,
       name: "Marketing Strategies",
       desc: "Learn how to create effective marketing campaigns.",
       faculty: "BBA",
